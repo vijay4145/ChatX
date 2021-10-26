87b423b2f8a92bfded3d7ecf671c768875577159
Ajay <gajay4145@gmail.com>
2021-10-26 10:43:32 +0530

    otp validation added

--- app/src/main/java/com/example/chatx/ui/OTP_verification.java
+++ app/src/main/java/com/example/chatx/ui/OTP_verification.java
@@ -2,29 +2,20 @@ package com.example.chatx.ui;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
-
 import com.example.chatx.MainActivity;
 import com.example.chatx.R;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.FirebaseException;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.PhoneAuthCredential;
-import com.google.firebase.auth.PhoneAuthOptions;
 import com.google.firebase.auth.PhoneAuthProvider;
-
-import java.util.concurrent.TimeUnit;
 
 public class OTP_verification extends AppCompatActivity {
     private String OTP;
